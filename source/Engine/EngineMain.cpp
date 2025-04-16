23589791fb9f12177d159bdd306803cfd4f17a13
BitKovin <39824091+BitKovin@users.noreply.github.com>
2025-04-16 04:09:09 +0300

    upd

--- source/Engine/EngineMain.cpp
+++ source/Engine/EngineMain.cpp
@@ -55,12 +55,12 @@ void EngineMain::initDemo()
     texture = AssetRegistry::GetTextureFromFile("GameData/cat.png");
 
 
-    for (size_t i = 0; i < 00; i++)
+    for (size_t i = 0; i < 00; i++) 
     {
         float angle = i * 0.1f; // Angle step controls tightness of the spiral
-        float radius = 5 + 0.3f * i; // Radius increases over time
-        float x = radius * cos(angle);
-        float y = 10;
+        float radius = 1 + 0.05f * i; // Radius increases over time
+        float x = radius * cos(angle) + 10;
+        float y = 50;
         float z = radius * sin(angle);
 
         Level::Current->AddEntity(new TestNpc(vec3(x, y, z)));
